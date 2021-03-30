c3a3855a9a7bb9cb85665912a54763631d2f87af
ryudongjae <rdj10149@gmail.com>
2021-03-30 16:35:41 +0900

    할인 정책(percent)

--- src/main/java/ryudongjae/core/discount/FixDiscountPolicy.java
+++ src/main/java/ryudongjae/core/discount/FixDiscountPolicy.java
@@ -5,10 +5,12 @@ import ryudongjae.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int discountFixAmountVip = 1000; //vip등급은 1000원 할인.
-    private int discountFixAmountGold =500;
 
-    @Override
+
+    private int discountFixAmountVip = 1000;//vip등급은 1000원 할인.
+    private int discountFixAmountGold = 500; //GOLD등급은 500원 할
+
+    @Override //할인정책(등급별 할인)
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
             return discountFixAmountVip;
