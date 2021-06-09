760c1c5b87bdd1089a1293c92d9a4d48b78ab077
ryudongjae <rdj10149@gmail.com>
2021-06-09 17:44:09 +0900

    Appconfig 주석추가

--- src/main/java/ryudongjae/core/AppConfig.java
+++ src/main/java/ryudongjae/core/AppConfig.java
@@ -9,6 +9,8 @@ import ryudongjae.core.member.MemberServiceImpl;
 import ryudongjae.core.member.MemoryMemberRepository;
 import ryudongjae.core.order.OrderService;
 import ryudongjae.core.order.OrderServiceImpl;
+
+//애플리케이션의 전체 동작 방식을 구성하기 위해,구성 객체를 생성하고 연결하는 책임을 가지는 별도의 설정 클래스
 @Configuration //appconfig를 구성정보로 사용한다 .@Bean이라 적힌 메서드를 모두 호출하고 반환된 객체를 스프링 컨테이너에 등록.
 public class AppConfig {
     @Bean
