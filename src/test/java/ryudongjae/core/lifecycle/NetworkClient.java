3bf577dbcf3029b0da86690387570edfe6f83c2b
ryudongjae <rdj10149@gmail.com>
2021-06-16 00:24:34 +0900

    빈 콜백 주기 (빈 등록 초기화, 소멸 메서드)

---
+++ src/test/java/ryudongjae/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package ryudongjae.core.lifecycle;
+
+public class NetworkClient {
+    private String url;
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url); }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+    //서비스 종료시 호출
+    public void disConnect() {
+        System.out.println("close + " + url);
+    }
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
+    }
+
+}
